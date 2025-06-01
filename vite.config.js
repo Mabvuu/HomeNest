2b7356d92ee83184b7212c5f986d26585d1980f5
Mabvuu <pattymabvuu@gmail.com>
2025-06-01 03:40:27 +0200

    Fix missing vite.svg

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+   base: '/homenest/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
